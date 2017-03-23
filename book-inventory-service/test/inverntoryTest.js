a70a9ef2074d7bf717a0ccae27b066a77bceaeca
Anekses <adrian.rydzynski@gmail.com>
2017-03-23 13:40:29 +0100

    pierwsze testy jednostkowe

---
+++ book-inventory-service/test/inverntoryTest.js
@@ -0,0 +1,20 @@
+// END 2 END
+
+const request = require('supertest');
+const app = require('../index');
+
+describe('Book inventory', function () {
+    it('allows to stock up the items', function (done) {
+        request(app)
+            .post('/stock')
+            .send({
+                isbn: "123123132",
+                count: 5
+            })
+            .expect({
+                isbn: "123123132",
+                count: 5
+            }, done);
+    });
+});
+
